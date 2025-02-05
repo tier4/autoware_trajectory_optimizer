9ff187e042c7bbf31308828f7a88609199197661
Daniel Sanchez <danielsanchezaran@gmail.com>
2025-02-05 14:07:15 +0900

    add new traj support
    Signed-off-by: Daniel Sanchez <danielsanchezaran@gmail.com>

--- include/autoware/trajectory_interpolator/trajectory_interpolator.hpp
+++ include/autoware/trajectory_interpolator/trajectory_interpolator.hpp
@@ -21,6 +21,7 @@
 #include <rclcpp/subscription.hpp>
 
 #include "nav_msgs/msg/odometry.hpp"
+#include <autoware_new_planning_msgs/msg/trajectories.hpp>
 #include <autoware_perception_msgs/msg/detail/predicted_objects__struct.hpp>
 #include <autoware_perception_msgs/msg/predicted_objects.hpp>
 #include <autoware_planning_msgs/msg/trajectory.hpp>
@@ -31,9 +32,11 @@
 namespace autoware::trajectory_interpolator
 {
 
+using autoware_new_planning_msgs::msg::Trajectories;
 using autoware_perception_msgs::msg::PredictedObjects;
 using autoware_planning_msgs::msg::Trajectory;
 using autoware_planning_msgs::msg::TrajectoryPoint;
+using NewTrajectory = autoware_new_planning_msgs::msg::Trajectory;
 using nav_msgs::msg::Odometry;
 
 class TrajectoryInterpolator : public rclcpp::Node
@@ -42,17 +45,19 @@ public:
   explicit TrajectoryInterpolator(const rclcpp::NodeOptions & options);
 
 private:
-  void on_traj(const Trajectory::ConstSharedPtr msg);
+  void on_traj(const Trajectories::ConstSharedPtr msg);
 
-  Trajectory interpolate_trajectory(
-    const Trajectory & input_trajectory, const Odometry & current_odometry);
+  NewTrajectory interpolate_trajectory(
+    const NewTrajectory & input_trajectory, const Odometry & current_odometry);
 
   static std::vector<TrajectoryPoint> clamp_negative_velocities(
     std::vector<TrajectoryPoint> & input_trajectory_array);
-  // interface publisher
-  rclcpp::Publisher<Trajectory>::SharedPtr traj_pub_;
+
   // interface subscriber
-  rclcpp::Subscription<Trajectory>::SharedPtr traj_sub_;
+  rclcpp::Subscription<Trajectories>::SharedPtr trajectories_sub_;
+  // interface publisher
+  rclcpp::Publisher<Trajectory>::SharedPtr traj_pub_;  // Rviz debug
+  rclcpp::Publisher<Trajectories>::SharedPtr trajectories_pub_;
 
   autoware::universe_utils::InterProcessPollingSubscriber<Odometry> sub_current_odometry_{
     this, "~/input/odometry"};
--- src/trajectory_interpolator.cpp
+++ src/trajectory_interpolator.cpp
@@ -31,18 +31,20 @@ TrajectoryInterpolator::TrajectoryInterpolator(const rclcpp::NodeOptions & optio
 : Node("trajectory_interpolator", options)
 {
   // interface subscriber
-  traj_sub_ = create_subscription<Trajectory>(
-    "mtr/trajectory", 10, std::bind(&TrajectoryInterpolator::on_traj, this, std::placeholders::_1));
+  trajectories_sub_ = create_subscription<Trajectories>(
+    "mtr/trajectories", 10,
+    std::bind(&TrajectoryInterpolator::on_traj, this, std::placeholders::_1));
   // interface publisher
   traj_pub_ = create_publisher<Trajectory>("smoothed/mtr/trajectory", 5);
+  trajectories_pub_ = create_publisher<Trajectories>("smoothed/mtr/trajectories", 5);
 }
 
-Trajectory TrajectoryInterpolator::interpolate_trajectory(
-  const Trajectory & input_trajectory, const Odometry & current_odometry)
+NewTrajectory TrajectoryInterpolator::interpolate_trajectory(
+  const NewTrajectory & input_trajectory, [[maybe_unused]] const Odometry & current_odometry)
 {
-  auto traj_points = autoware::motion_utils::convertToTrajectoryPointArray(input_trajectory);
+  auto traj_points = input_trajectory.points;
   // guard for invalid trajectory
-  traj_points = autoware::motion_utils::removeOverlapPoints(traj_points);
+  // traj_points = autoware::motion_utils::removeOverlapPoints(traj_points);
   if (traj_points.size() < 2) {
     RCLCPP_ERROR(get_logger(), "No enough points in trajectory after overlap points removal");
     return input_trajectory;
@@ -50,51 +52,53 @@ Trajectory TrajectoryInterpolator::interpolate_trajectory(
 
   clamp_negative_velocities(traj_points);
 
-  constexpr double nearest_dist_threshold = 2.0;
-  constexpr double nearest_yaw_threshold = 1.0;  // [rad]
+  // constexpr double nearest_dist_threshold = 2.0;
+  // constexpr double nearest_yaw_threshold = 1.0;  // [rad]
 
-  // Arc length from the initial point to the closest point
-  const auto current_pose = current_odometry.pose.pose;
+  // // Arc length from the initial point to the closest point
+  // const auto current_pose = current_odometry.pose.pose;
 
-  const size_t current_seg_idx =
-    autoware::motion_utils::findFirstNearestSegmentIndexWithSoftConstraints(
-      traj_points, current_pose, nearest_dist_threshold, nearest_yaw_threshold);
-  const double negative_front_arclength_value = autoware::motion_utils::calcSignedArcLength(
-    traj_points, current_pose.position, current_seg_idx, traj_points.back().pose.position, 0);
-  const auto front_arclength_value = std::fabs(negative_front_arclength_value);
+  // const size_t current_seg_idx =
+  //   autoware::motion_utils::findFirstNearestSegmentIndexWithSoftConstraints(
+  //     traj_points, current_pose, nearest_dist_threshold, nearest_yaw_threshold);
+  // const double negative_front_arclength_value = autoware::motion_utils::calcSignedArcLength(
+  //   traj_points, current_pose.position, current_seg_idx, traj_points.back().pose.position, 0);
+  // const auto front_arclength_value = std::fabs(negative_front_arclength_value);
 
-  std::vector<double> out_arclength;
-  // Step1. Resample front trajectory
-  constexpr double front_ds = 0.1;
-  for (double ds = 0.0; ds <= front_arclength_value; ds += front_ds) {
-    out_arclength.push_back(ds);
-  }
-  if (std::fabs(out_arclength.back() - front_arclength_value) < 1e-3) {
-    out_arclength.back() = front_arclength_value;
-  } else {
-    out_arclength.push_back(front_arclength_value);
-  }
+  // std::vector<double> out_arclength;
+  // // Step1. Resample front trajectory
+  // constexpr double front_ds = 0.1;
+  // for (double ds = 0.0; ds <= front_arclength_value; ds += front_ds) {
+  //   out_arclength.push_back(ds);
+  // }
+  // if (std::fabs(out_arclength.back() - front_arclength_value) < 1e-3) {
+  //   out_arclength.back() = front_arclength_value;
+  // } else {
+  //   out_arclength.push_back(front_arclength_value);
+  // }
 
-  auto output_traj = autoware::motion_utils::resampleTrajectory(
-    autoware::motion_utils::convertToTrajectory(traj_points), out_arclength, true, true, false);
+  // auto output_traj = autoware::motion_utils::resampleTrajectory(
+  //   autoware::motion_utils::convertToTrajectory(traj_points), out_arclength, true, true, false);
 
-  // output_traj =
-  //   autoware::motion_utils::resampleTrajectory(output_traj, 0.1, true, true, true, true);
-  std::cerr << "Resampled trajectory size: " << output_traj.points.size() << std::endl;
-  std::cerr << "out_arc_length: " << out_arclength.back() << std::endl;
+  // // output_traj =
+  // //   autoware::motion_utils::resampleTrajectory(output_traj, 0.1, true, true, true, true);
+  // std::cerr << "Resampled trajectory size: " << output_traj.points.size() << std::endl;
+  // std::cerr << "out_arc_length: " << out_arclength.back() << std::endl;
 
-  int i = 0;
-  for (auto & point : output_traj.points) {
-    std::cerr << "point(" << i++
-              << ") x: " << point.pose.position.x - output_traj.points.front().pose.position.x
-              << " y: " << point.pose.position.y - output_traj.points.front().pose.position.y
-              << std::endl;
-  }
-  output_traj.header = input_trajectory.header;
+  // int i = 0;
+  // for (auto & point : output_traj.points) {
+  //   std::cerr << "point(" << i++
+  //             << ") x: " << point.pose.position.x - output_traj.points.front().pose.position.x
+  //             << " y: " << point.pose.position.y - output_traj.points.front().pose.position.y
+  //             << std::endl;
+  // }
+
+  NewTrajectory output_traj = input_trajectory;
+  output_traj.points = traj_points;
   return output_traj;
 }
 
-void TrajectoryInterpolator::on_traj(const Trajectory::ConstSharedPtr msg)
+void TrajectoryInterpolator::on_traj([[maybe_unused]] const Trajectories::ConstSharedPtr msg)
 {
   current_odometry_ptr_ = sub_current_odometry_.takeData();
   if (!current_odometry_ptr_) {
@@ -102,8 +106,19 @@ void TrajectoryInterpolator::on_traj(const Trajectory::ConstSharedPtr msg)
     return;
   }
 
-  const auto output_traj = interpolate_trajectory(*msg, *current_odometry_ptr_);
-  traj_pub_->publish(output_traj);
+  Trajectories output_trajectories = *msg;
+  output_trajectories.trajectories.clear();
+  for (auto & trajectory : msg->trajectories) {
+    auto out_trajectory = interpolate_trajectory(trajectory, *current_odometry_ptr_);
+    output_trajectories.trajectories.push_back(out_trajectory);
+  }
+
+  trajectories_pub_->publish(output_trajectories);
+
+  Trajectory sample_trajectory;
+  sample_trajectory.points = output_trajectories.trajectories.front().points;
+  sample_trajectory.header = output_trajectories.trajectories.front().header;
+  traj_pub_->publish(sample_trajectory);
 }
 
 std::vector<TrajectoryPoint> TrajectoryInterpolator::clamp_negative_velocities(
